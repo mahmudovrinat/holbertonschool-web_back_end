845cfd512f8ddb99c0898222fe69bbb45226b37d
root <mahmudovrinat88@gmail.com>
2025-07-11 19:19:31 +0000

    commit

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -15,7 +15,6 @@ def log_stats(mongo_collection):
     status_count = mongo_collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_count} status check")
 
-
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
